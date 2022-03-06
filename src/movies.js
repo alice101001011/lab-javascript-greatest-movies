0988f9f999a9fd29e308539ae4ab19c28380fa79
alice101001011 <alice101001011@gmail.com>
2022-03-06 23:37:08 +0100

    iteration 7

--- src/movies.js
+++ src/movies.js
@@ -1,60 +1,127 @@
+// Added movies array and console.logs to test functions in console
+
+const movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    director: 'Frank Darabont',
+    duration: '2h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9.3
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    director: 'Francis Ford Coppola',
+    duration: '2h 55min',
+    genre: ['Crime', 'Drama'],
+    score: 9.2
+  },
+  {
+    title: 'The Godfather: Part II',
+    year: 1974,
+    director: 'Francis Ford Coppola',
+    duration: '3h 22min',
+    genre: ['Crime', 'Drama'],
+    score: 9
+  },
+  {
+    title: 'The Dark Knight',
+    year: 2008,
+    director: 'Christopher Nolan',
+    duration: '2h 32min',
+    genre: ['Action', 'Crime', 'Drama', 'Thriller'],
+    score: 9
+  },
+  {
+    title: '12 Angry Men',
+    year: 1957,
+    director: 'Sidney Lumet',
+    duration: '1h 36min',
+    genre: ['Crime', 'Drama'],
+    score: 8.9
+  },
+  {
+    title: 'Schindler"s List',
+    year: 1993,
+    director: 'Steven Spielberg',
+    duration: '3h 15min',
+    genre: ['Biography', 'Drama', 'History'],
+    score: 8.9
+  },
+  {
+    title: 'Pulp Fiction',
+    year: 1994,
+    director: 'Quentin Tarantino',
+    duration: '2h 34min',
+    genre: ['Crime', 'Drama'],
+    score: 8.9
+  }
+];
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(anyArr) {
 
+function getAllDirectors(anyArr) {
   const movieDirectors = anyArr.map((anyObj) => anyObj.director);
 
   return movieDirectors;
 }
 
+console.log('Iteration 1 – getAllDirectors:', getAllDirectors(movies));
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(anyArr) {
-
-  const moviesGenreDirector = anyArr.filter((anyObj) => anyObj.director === "Steven Spielberg" && anyObj.genre.includes("Drama"));
+  const moviesGenreDirector = anyArr.filter(
+    (anyObj) =>
+      anyObj.director === 'Steven Spielberg' && anyObj.genre.includes('Drama')
+  );
 
   return moviesGenreDirector.length;
 }
 
+console.log('Iteration 2 – howManyMovies:', howManyMovies(movies));
+
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(anyArr) {
-
   if (!anyArr.length) {
     return 0;
-  } 
+  }
 
   const sumScores = anyArr.reduce((accumulator, currentValue) => {
-    return (accumulator + currentValue.score)}, 0);
+    return accumulator + currentValue.score;
+  }, 0);
 
-  return Math.round((sumScores / anyArr.length) * 100)/100;
-
+  return Math.round((sumScores / anyArr.length) * 100) / 100;
 }
+
+console.log('Iteration 3 – scoresAverage:', scoresAverage(movies));
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(anyArr) {
+  const arrDrama = anyArr.filter((anyObj) => anyObj.genre.includes('Drama'));
 
-  const arrDrama = anyArr.filter((anyObj) => anyObj.genre.includes("Drama"));
-  
-  const sumScoresDrama = arrDrama.reduce((accumulator, currentValue) => 
-    {return accumulator + currentValue.score}, 0);
+  const sumScoresDrama = arrDrama.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.score;
+  }, 0);
 
   if (!arrDrama.length) {
     return 0;
   }
 
-  return Math.round((sumScoresDrama / arrDrama.length) * 100)/100;
-
+  return Math.round((sumScoresDrama / arrDrama.length) * 100) / 100;
 }
+
+console.log('Iteration 4 – dramaMoviesScore:', dramaMoviesScore(movies));
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(anyArr) {
-
   const moviesByYear = [...anyArr];
 
-  moviesByYear.sort((a,b) => {
-
+  moviesByYear.sort((a, b) => {
     if (a.year === b.year) {
-      return a.title.localeCompare(b.title)
+      return a.title.localeCompare(b.title);
     }
 
     return a.year - b.year;
@@ -63,27 +130,40 @@ function orderByYear(anyArr) {
   return moviesByYear;
 }
 
+console.log('Iteration 5 – orderByYear:', orderByYear(movies));
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(anyArr) {
-
-  const movieTitlesArr = anyArr.map(anyObj => anyObj.title);
+  const movieTitlesArr = anyArr.map((anyObj) => anyObj.title);
   const movieTitlesAlphabetical = movieTitlesArr.sort().slice(0, 20);
 
   return movieTitlesAlphabetical;
 }
 
+console.log('Iteration 6 – orderAlphabetically:', orderAlphabetically(movies));
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(anyArr) {
+  let newArr = [...anyArr];
 
-  let moviesInMinutes = anyArr.map((anyObj) => {
-    
-  })
+  let hoursMinsToMinutes = newArr.map((anyObj) => {
+    const durationArr = anyObj.duration.split(' ');
+    const hours = Number(durationArr[0].replace(/[^0-9]/g, ''));
+    const minutes = durationArr[1]
+      ? Number(durationArr[1].replace(/[^0-9]/g, ''))
+      : 0;
+    const duration = hours * 60 + minutes;
+
+    return { newArr, duration };
+  });
+
+  return hoursMinsToMinutes;
 }
+
+console.log('Iteration 7 – turnHoursToMinutes:', turnHoursToMinutes(movies));
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -96,6 +176,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
